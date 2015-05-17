5536c3a7054acea5508782a9708e47b7372c70a3
Rares Smeu <raressmeu@yahoo.com>
2015-05-17 18:54:40 +0300

    Added Logger.

--- src/main/java/ro/unitbv/news/Main.java
+++ src/main/java/ro/unitbv/news/Main.java
@@ -1,7 +1,20 @@
 package ro.unitbv.news;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Rares Smeu
  */
 public class Main {
+
+	private final static Logger log = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		log.trace("trace");
+		log.debug("debug");
+		log.info("info");
+		log.error("error");
+	}
 }
